3cf3ca32b3e53ec519317478f2b1e8ccceeedc21
josuehasspereira <josue.hass@hotmail.com>
2023-05-11 22:25:56 -0300

    Add +bots

--- bot2.py
+++ bot2.py
@@ -1,54 +1,81 @@
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 
-# Definindo as informações da placa de vídeo que queremos procurar
-modelo = 'RTX 2060'
-capacidade = '6GB'
-preco_maximo = 1150
+# Caminho do ChromeDriver
+caminho_chromedriver = r"C:\Users\Josué\Documents\chromedriver2\chromedriver.exe"
 
-# Definindo a URL da página de busca do Marketplace do Facebook
-url = f'https://www.facebook.com/marketplace/108420019205709/search/?query={modelo} {capacidade}'
+url = f'https://www.facebook.com'
+urlMarket = f'https://www.facebook.com/marketplace/joinville/search?minPrice=900&maxPrice=1200&query=RTX%202060&exact=false'
 
-# Configurando o driver do Chrome
-chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
-driver_path = r'C:\Users\Josué\Documents\chromedriver2\chromedriver.exe'
+# Configurar as opções do ChromeDriver
+opcoes = webdriver.ChromeOptions()
+# Comente a próxima linha se quiser que o Chrome seja aberto em modo headless
+opcoes.headless = False
 
-driver_path = r'C:\Users\Josué\Documents\chromedriver2\chromedriver.exe'
+# Criar uma instância do ChromeDriver
+driver = webdriver.Chrome(executable_path=caminho_chromedriver, options=opcoes)
 
-
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_path
-service = webdriver.chrome.service.Service(executable_path=driver_path)
-driver = webdriver.Chrome(executable_path=driver_path, options=options)
-
-# Navegando para a página de busca do Marketplace do Facebook
+# Acessar a página do Mercado Livre
 driver.get(url)
 
-# Aguardando o carregamento completo da página
-time.sleep(10)
+# Esperar a página carregar
+driver.implicitly_wait(10)
 
-# Obtendo todos os resultados da busca
-results = driver.find_elements_by_xpath('//div[@data-testid="marketplace_feed_item"]')
+# Navega até a página de login do Facebook
+driver.get("https://www.facebook.com/login")
 
-# Criando uma lista para armazenar os resultados encontrados
-anuncios = []
+# Insere as informações de usuário e senha
+username_field = driver.find_element(By.CSS_SELECTOR, 'input#email')
+username_field.send_keys("josue.hass@hotmail.com")
 
-# Iterando pelos resultados e adicionando à lista os que atendem aos critérios de preço
-for result in results:
-    try:
-        preco = float(result.find_element_by_class_name('a-price').text.replace('.', '').replace(',', '.'))
-        if preco <= preco_maximo:
-            anuncios.append(result.find_element_by_tag_name('a').get_attribute('href'))
-        if len(anuncios) == 5:
-            break
-    except:
-        continue
+password_field = driver.find_element(By.CSS_SELECTOR, 'input#pass')
+password_field.send_keys("cleopatrap4$Sw0rD()")
 
-# Abrindo os anúncios no navegador Chrome
-for anuncio in anuncios:
-    driver.execute_script(f"window.open('{anuncio}');")
+# Clica no botão de login
+login_button = driver.find_element(By.XPATH, "//button[contains(., 'Entrar') or contains(., 'Log In') or contains(., 'Log in') or contains(., 'Entrar com e-mail')] | //input[@type='submit']")
+login_button.click()
 
-# Fechando o driver do Chrome
+driver.implicitly_wait(10)
+
+driver.execute_script(f"window.open('{urlMarket}', '_blank');")
+
+# Alternar para a nova guia
+driver.switch_to.window(driver.window_handles[1])
+
+# Obter a URL do resultado
+url_resultado = driver.current_url
+
+# Realizar alguma operação com cada resultado, por exemplo, imprimir a URL
+print('URL do resultado:', url_resultado)
+
+# Alternar de volta para a guia anterior
+driver.switch_to.window(driver.window_handles[1])
+
+text = "/marketplace/item/"
+
+elementsLinks = driver.find_elements(By.XPATH,'//a[contains(@href, "%s")]' % text)
+print("//-----------------------------//")
+i = 5
+#for i in range(10):
+#    driver.execute_script(f"window.open('{elementsLinks[i].get_attribute('href')}', '_blank');")
+driver.execute_script(f"window.open('{elementsLinks[i].get_attribute('href')}', '_blank');")
+
+driver.implicitly_wait(20)
+
+text = "Enviar"
+#div[aria-label='Enviar']
+#id="facebook"
+enviar_mensagem = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.XPATH, '//span[@data-icon="send"]')))
+
+print("*******")
+print(enviar_mensagem)
+
+enviar_mensagem.click()
+
+input("Pressione enter")
 driver.quit()
+quit()
